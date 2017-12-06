3425688602debd81e87092f7b1c73d7a297f713d
MrHidalgo <Luckyhidalgo@gmail.com>
2017-12-06 13:24:32 +0200

    TestSite: 4. Replace files place. 5. Add handlebars lib. 6. Create routing for the current static page. 7. Testing routing data to the page.

--- TestSite/testsite.js
+++ TestSite/testsite.js
@@ -2,47 +2,75 @@ const express = require('express'),
 	handlebars = require('express-handlebars').create({
 		defaultLayout: 'main'
 	});
-
 const app = express();
+
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+
 app.set('port', process.env.PORT || 3000);
+
+/**
+ * Create path to static files
+ */
+app.use(express.static(__dirname + '/public'));
+
+
+/* Some test */
+// let fortunes = [
+// 	"Some fortunes 0",
+// 	"Some fortunes 1",
+// 	"Some fortunes 2",
+// 	"Some fortunes 3",
+// 	"Some fortunes 4",
+// 	"Some fortunes 5"
+// ];
 
 /**
  * Home page
  */
 app.get('/', (request, response) => {
-	response.type('text/plain');
-	response.send("Home page site");
+	// response.type('text/plain');
+	// response.send("Home page site");
+	response.render('home');
 });
-
 /**
  * About page
  */
 app.get('/about', (request, response) => {
-	response.type('text/plain');
-	response.send("About site");
+	// response.type('text/plain');
+	// response.send("About site");
+
+	// let randomFortune = Math.floor(Math.random() * fortunes.length);
+
+    // response.render('about', {
+     //    fortune: fortunes[randomFortune]
+	// });
+
+    response.render('about');
 });
+
 
 /**
  * User page 404
  */
 app.use((request, response) => {
-	response.type('text/plain');
+	// response.type('text/plain');
 	response.status(404);
-	response.send('404 - Not Found');
+	// response.send('404 - Not Found');
+    response.render('404');
 });
-
 /**
  * User page 500
  */
 app.use((request, response) => {
-	response.type('text/plain');
+	// response.type('text/plain');
 	response.status(500);
-	response.send('500 - Server Error');
+	// response.send('500 - Server Error');
+    response.render('500');
 });
+
 
 /**
  * Start to listen port 3000
